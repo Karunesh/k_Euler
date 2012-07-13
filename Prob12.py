8adba35dcdf50d7291436d08defb2cb97d7871d5
Karunesh Kaushal <k1821kaushal@gmail.com>
2012-07-13 23:48:06 +0530

    This seems too slow. Gotta try something new.
    Signed-off-by: Karunesh Kaushal <k1821kaushal@gmail.com>

--- Prob12.py
+++ Prob12.py
@@ -1,4 +1,5 @@
 # prob12.py
+import math
 
 def getNumFactors(number):
     """
@@ -9,13 +10,16 @@ def getNumFactors(number):
     numFactors = 2 # Already accounting for 1 and number itself.
 
     i = 2
-    while i < number:
+    for i in range(2, int(number / 2)):
         if (number % i) == 0:
             numFactors += 1
 
         i += 1
-        
+
+    print("num := ", number, "     numFactors := ", numFactors)    
     return numFactors
+
+# end getNumFactors
 
 def main():
     currentTriNum = 0
@@ -25,11 +29,11 @@ def main():
     while numFactors <= 500:
         currentTriNum += currentNum
         currentNum += 1
-        print(currentTriNum, ", ", currentNum)
+        #print(currentTriNum, ", ", currentNum)
         numFactors = getNumFactors(currentTriNum)
     else:
         print("currentTriNum := ", currentTriNum)
-  
+
 # end main
 
 if __name__ == "__main__":
